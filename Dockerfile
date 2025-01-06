292e5a00b8a17fd328d0ded5af252aa6dffec2b4
renatofilippelli <renatofilippelli@gmail.com>
2025-01-06 08:05:18 -0300

    chore: docker compose created

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,20 @@ FROM node:23-slim
 
 RUN apt update && \
     apt install -y curl && \
-    npm install -g nodemon
+    npm install -g ts-node typescript
+
+COPY start.sh /
+RUN chmod +x /start.sh
 
 USER node
+
+WORKDIR /home/node/app
+
+#COPY package.json ./
+#RUN npm install
+
+EXPOSE 3000
+
+# CMD ["sh", "-c", "npm install && tail -f /dev/null"]
+
+CMD ["/start.sh"]
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
-console.log('Happy developing ✨')
+import express from 'express';
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const port = 3000;
+
+// Create a connection to the MySQL database
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: 3306,
+});
+
+// Test the database connection
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the MySQL database.');
+});
+
+// Define an endpoint
+app.get('/data', (req, res) => {
+    connection.query('SELECT NOW();', (error, results) => {
+        if (error) {
+            return res.status(500).send(error);
+        }
+        res.json(results);
+    });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
